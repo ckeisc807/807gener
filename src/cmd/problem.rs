d025d0e3a1009f54523bcf0f85e72f7b84992397
ckeisc807 <s10855168@gmail.com>
2024-03-19 15:03:40 +0800

    copy when creating problem

--- src/cmd/problem.rs
+++ src/cmd/problem.rs
@@ -2,9 +2,34 @@ use crate::Hub;
 
 use super::Problem;
 use super::Cmd;
-use std::path::PathBuf;
-use std::io;
+use std::path::{PathBuf, Path};
+use std::{io, fs};
 
+fn copy_directory(source: &Path, destination: &Path) -> io::Result<()> {
+    // 创建目标文件夹
+    fs::create_dir_all(destination)?;
+
+    // 遍历源文件夹中的内容
+    for entry in fs::read_dir(source)? {
+        let entry = entry?;
+        let source_path = entry.path();
+        let file_name = entry.file_name();
+
+        // 构建目标路径
+        let mut destination_path = PathBuf::from(destination);
+        destination_path.push(&file_name);
+
+        // 如果是目录，则递归复制文件夹
+        if source_path.is_dir() {
+            copy_directory(&source_path, &destination_path)?;
+        } else {
+            // 如果是文件，则复制文件
+            fs::copy(&source_path, &destination_path)?;
+        }
+    }
+
+    Ok(())
+}
 
 impl Cmd {
     pub fn problem(&mut self) {
@@ -40,8 +65,13 @@ impl Cmd {
 
         let hub: &mut Hub = &mut self.hub.as_mut().expect("hub not found");
         let mut problem_path = PathBuf::from(&hub.dir_path);
-
+        let mut default_path = PathBuf::from(&hub.dir_path);
+        default_path.push("config");
+        default_path.push("default");
         problem_path.push(problem_id.to_string());
+
+        copy_directory(&default_path, &problem_path)
+            .expect("Failed to copy from default");
 
         let problem = Problem {
             is_empty: false,
